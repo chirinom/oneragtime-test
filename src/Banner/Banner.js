5a82bb6b042ee1dfb9277153741fd79b7ced0076
Miguel Chirino <mchirinot@gmail.com>
2021-04-26 16:31:27 +0200

    banner fix

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -16,7 +16,7 @@ export default function Banner() {
       <div className="logoWrapper"/>
       <div className={isWide ? "banner-text home-card wide" : "banner-text home-card"}>
         {show ? null : <h1 className="header">Welcome to OneRagtime</h1>}
-        {show ? null : <p className="content">Amett minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>}
+        {show ? null : <p className="content">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint</p>}
         {show ? null : <button 
           className="animation-btn" 
           onClick={() => {
